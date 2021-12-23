827aa16de0003bae765b4c7ce089134836e30da3
RDDigital <94842732+RamDevIO1@users.noreply.github.com>
2021-12-23 08:37:01 +0000

    index

--- public/js/editor.js
+++ public/js/editor.js
@@ -18,62 +18,59 @@ uploadInput.addEventListener('change', () => {
 })
 
 const uploadImage = (uploadFile, uploadType) => {
-    const [file] = uploadFile.files;
-    if(file && file.type.includes("image")){
-        const formdata = new FormData();
-        formdata.append('image', file);
+  const [file] = uploadFile.files;
+  if(file && file.type.includes("image")){
+    const formdata = new FormData();
+    formdata.append('image', file);
 
-        fetch('/upload', {
-            method: 'post',
-            body: formdata
-        }).then(res => res.json())
-        .then(data => {
-            if(uploadType == "image"){
-                addImage(data, file.name);
-            } else{
-                bannerPath = `${location.origin}/${data}`;
-                banner.style.backgroundImage = `url("${bannerPath}")`;
-            }
-        })
-    } else{
-        alert("upload Image only");
-    }
+    fetch('/upload', {
+        method: 'post',
+        body: formdata
+    }).then(res => res.json()).then(data => {
+      if(uploadType == "image"){
+          addImage(data, file.name);
+      } else{
+          bannerPath = `${location.origin}/${data}`;
+          banner.style.backgroundImage = `url("${bannerPath}")`;
+      }
+    })
+  } else{
+      alert("upload Image only");
+  }
 }
 
 const addImage = (imagepath, alt) => {
-    let curPos = articleFeild.selectionStart;
-    let textToInsert = `\r![${alt}](${imagepath})\r`;
-    articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
+  let curPos = articleFeild.selectionStart;
+  let textToInsert = `\r![${alt}](${imagepath})\r`;
+  articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
 }
 
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 publishBtn.addEventListener('click', () => {
-    if(articleFeild.value.length && blogTitleField.value.length){
-        // generating id
-        let letters = 'abcdefghijklmnopqrstuvwxyz';
-        let blogTitle = blogTitleField.value.split(" ").join("-");
-        let id = '';
-        for(let i = 0; i < 4; i++){
-            id += letters[Math.floor(Math.random() * letters.length)];
-        }
-
-        // setting up docName
-        let docName = `${blogTitle}-${id}`;
-        let date = new Date(); // for published at info
-
-        //access firstore with db variable;
-        db.collection("blogs").doc(docName).set({
-            title: blogTitleField.value,
-            article: articleFeild.value,
-            bannerImage: bannerPath,
-            publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
-        })
-        .then(() => {
-            location.href = `/${docName}`;
-        })
-        .catch((err) => {
-            console.error(err);
-        })
+  if(articleFeild.value.length && blogTitleField.value.length){
+    // generating id
+    let letters = '1234567890';
+    let blogTitle = blogTitleField.value.split(" ").join("-");
+    let id = '';
+    for(let i = 0; i < 7; i++){
+        id += letters[Math.floor(Math.random() * letters.length)];
     }
+
+    // setting up docName
+    let docName = `${blogTitle}-${id}`;
+    let date = new Date(); // for published at info
+
+    //access firstore with db variable;
+    db.collection("blogs").doc(docName).set({
+        title: blogTitleField.value,
+        article: articleFeild.value,
+        bannerImage: bannerPath,
+        publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+    }).then(() => {
+        location.href = `/${docName}`;
+    }).catch((err) => {
+        console.error(err);
+    })
+  }
 })
