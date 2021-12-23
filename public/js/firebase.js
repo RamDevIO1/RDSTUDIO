2bad39d6f6e9b3e46a308fb1e282a7b9b9adbee8
RamDevIO1 <ramdev.io.id@gmail.com>
2021-12-23 09:20:38 +0700

    .

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyD5El91OZZTakD3bDVWMj2HkLrKHcor1AY",
+  authDomain: "rdstudio-29270.firebaseapp.com",
+  projectId: "rdstudio-29270",
+  storageBucket: "rdstudio-29270.appspot.com",
+  messagingSenderId: "976125699729",
+  appId: "1:976125699729:web:8608a9ddce73fd064c0130",
+  measurementId: "G-TJKW2HBHZ7"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
